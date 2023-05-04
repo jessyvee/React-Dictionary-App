918febb89575d93d8e2cf022d9cfca9cc303c0bb
Jesse <jessyvee1@gmail.com>
2023-05-05 01:45:45 +0300

    assignment completed

---
+++ dictionary-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Dictionary from './components/Dictionary';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Dictionary/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
